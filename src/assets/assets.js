1e054c324f0233c37f741ae1087eac5bf1e8429f
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2024-10-10 11:03:44 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,283 @@
+
+import kid from './kid.jpeg'
+import kid1 from './kid1.jpeg'
+import kid3 from './kid3.jpeg' 
+import th22 from './th 22.jpeg'
+import search from './search.jpeg'
+import profile from './profile.jpeg'
+import cart from './cart.jpeg'
+import menu from './menu.jpeg'
+import drop from './drop.jpeg'
+import hero from './hero.jpeg'
+import men1 from './men1.jpeg'
+import exchange from './exchange.jpeg'
+import support from './support.jpeg'
+import policy from './policy.jpeg'
+export const assets = {
+  kid,
+  kid1,
+  kid3,
+  th22,
+  search,
+  profile,
+  cart,
+  menu,
+  drop,
+  hero,
+  men1,
+  exchange,
+  policy,
+  support
+}
+
+
+export const products = [
+    {
+        id:"aaaaa",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+        Price:100,
+        image:[kid],
+        category:"women",
+        subCategory: "Topwear",
+        sizes:["s","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+      id:"aaaab",
+      name: "Women Round Neck Cotton Top",
+      description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+      Price:200,
+      image:[men1],
+      category:"women",
+      subCategory: "Topwear",
+      sizes:["s","M","L"],
+      date: 1716634345448,
+      bestseller: true
+  },{
+    id:"aaaab",
+    name: "Women Round Neck Cotton Top",
+    description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+    Price:200,
+    image:[kid],
+    category:"women",
+    subCategory: "Topwear",
+    sizes:["s","M","L"],
+    date: 1716634345448,
+    bestseller: true
+},{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+},
+{
+  id:"aaaab",
+  name: "Women Round Neck Cotton Top",
+  description: "A lightweight, usually Knitted,pullover shirt,close-fitting and",
+  Price:200,
+  image:[kid],
+  category:"women",
+  subCategory: "Topwear",
+  sizes:["s","M","L"],
+  date: 1716634345448,
+  bestseller: true
+}
+]
